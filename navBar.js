35e1788a34b9176104980f18eea4eacadabbf4c3
candymorgan <candyaz1000@gmail.com>
2023-07-13 09:21:08 -0700

    landing page

---
+++ navBar.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+   
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.marginTop = "78px";
+      x.style.backgroundColor = "#ffff";
+      x.style.position = "absolute";
+      x.style.left = "60%";
+      x.style.width = "100%"
+    }
+
+
+  }
