c9f031b10b0caabb46dd7611c5c8de80d927f2a7
Roni <ronniiii@yahoo.com>
2023-07-27 18:49:18 +0100

    [RESTRUCTURE]Restructure file structure

--- js/gallery.js
+++ js/gallery.js
@@ -1,93 +1,93 @@
 const imagesData = [
     {
-      img: "./images/cel1.jpeg",
+      img: "../images/cel1.jpeg",
       name: "Nelly Cheiboi",
       descripiton:
         "Chimamanda ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
     },
     {
-      img: "./images/cel2.jpeg",
+      img: "../images/cel2.jpeg",
       name: "Monica Katebe Musonda",
       descripiton:
         "Okafor ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
     },
     {
-      img: "./images/cel3.jpeg",
+      img: "../images/cel3.jpeg",
       name: "Catherine Lesetedi",
       descripiton:
         "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
     },
     {
-        img: "./images/cel4.jpeg",
+        img: "../images/cel4.jpeg",
         name: "Oluwatosin Olaseinde",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel5.jpeg",
+        img: "../images/cel5.jpeg",
         name: "Diarra Bousso",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel6.jpeg",
+        img: "../images/cel6.jpeg",
         name: "Gisela Van Houcke",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel7.jpeg",
+        img: "../images/cel7.jpeg",
         name: "Lumbie Mlambo",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel8.jpeg",
+        img: "../images/cel8.jpeg",
         name: "Alice Mamaga AmoAko",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel9.jpeg",
+        img: "../images/cel9.jpeg",
         name: "Maria Stevens",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel10.jpeg",
+        img: "../images/cel10.jpeg",
         name: "Aisha Pandor",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel11.jpeg",
+        img: "../images/cel11.jpeg",
         name: "Charlette N'Guessan",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
       {
-        img: "./images/cel12.jpeg",
+        img: "../images/cel12.jpeg",
         name: "Sandra Chukwudozie",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
 
       {
-        img: "./images/cel13.jpeg",
+        img: "../images/cel13.jpeg",
         name: "Sandra Mwiihangele",
         descripiton:
           "Jane ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump,Bacon ipsum dolor amet short loin drumstick landjaeger tail rump",
       },
 
       {
-        img: "./images/cel14.jpeg",
+        img: "../images/cel14.jpeg",
         name: "Awamary Lowe-Khan",
         descripiton:
           "CEO Awamary Lowe-Khan who hails from Gambia is on a mission to build the next generation of women leaders in Africa, by focusing on innovative ways to help them flourish. In 2018, Awamary returned to The Gambia, driven by a deep desire to effect transformative change. She established The Woman Boss and The Disruptive Lab under the Innovate Gambia umbrella. The Woman Boss is dedicated to empowering women and girls by fostering entrepreneurship, innovation, and leadership. With a focus on tackling societal challenges such as gender-based violence and traditional gender roles.",
       },
 
       {
-        img: "./images/cel15.jpeg",
+        img: "../images/cel15.jpeg",
         name: "Wassa Inoni",
         descripiton:
           "CEO Awamary Lowe-Khan who hails from Gambia is on a mission to build the next generation of women leaders in Africa, by focusing on innovative ways to help them flourish. In 2018, Awamary returned to The Gambia, driven by a deep desire to effect transformative change. She established The Woman Boss and The Disruptive Lab under the Innovate Gambia umbrella. The Woman Boss is dedicated to empowering women and girls by fostering entrepreneurship, innovation, and leadership. With a focus on tackling societal challenges such as gender-based violence and traditional gender roles.",
